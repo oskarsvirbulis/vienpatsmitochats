b92b8158fccd1660075487f092268635c1d9d35b
Hozei <87283148+oskarsvirbulis@users.noreply.github.com>
2023-02-07 10:53:27 +0200

    q

---
+++ skripti.js
@@ -0,0 +1,11 @@
+//alert("čau")
+console.log("test")
+
+var logs = document.querySelector(".chataZinas");
+var vards = document.querySelector("vards");
+var zina = document.querySelector("zina");
+
+function sutitZinu(){
+    console.log("Poga darbulas");
+    console.log("ziņu ir: "+zina.value);
+}
