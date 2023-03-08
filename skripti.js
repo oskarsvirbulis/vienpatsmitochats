86a1811d8c88d37d3c5f0b8e3e2faff41fe39e2d
Hozei <87283148+oskarsvirbulis@users.noreply.github.com>
2023-03-08 10:27:32 +0200

    os

--- skripti.js
+++ skripti.js
@@ -1,6 +1,7 @@
 //alert("čau")
 console.log("test")
 
+const API = "https://stuta-zinas-json-11.oskarsvirbulis.repl.co"
 var logs = document.querySelector(".chataZinas");
 var vards = document.querySelector("#vards");
 var zina = document.querySelector("#zina");
@@ -10,10 +11,11 @@ function sutitZinu(){
     console.log("ziņu ir: "+zina.value);
 
     logs.innerHTML =logs.innerHTML +"</br>"+ zina.value;
+    fetch (API + '/sutit/' + vards.value + "</>" + zina.value)
 }
 
 async function ieladetZinas(){
-    let datiNoServera = await fetch("zinas.txt");
+    let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.text();
     console.log(dati); 
     logs.innerHTML = dati;
